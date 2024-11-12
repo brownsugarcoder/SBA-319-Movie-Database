e1f567aa1df2fe7fa476ee8efad67eadafb9d92c
ShaVon H. <144467852+brownsugarcoder@users.noreply.github.com>
2024-11-11 19:17:56 -0500

    Add files via upload

---
+++ Data/comments.mjs
@@ -0,0 +1,139 @@
+
+// Generated to populate mongodb. Wasn't used for application
+
+
+[
+    {
+        "name": "Kevin Scott",
+        "email": "zhahn@gmail.com",
+        "movie_id": "e98aa2eb-cb93-4bf9-8577-04da114572a1",
+        "text": "Step couple interesting defense past minute author if. Better sure bad future a.",
+        "date": "2020-09-04T20:45:04"
+    },
+    {
+        "name": "Miranda Trevino",
+        "email": "revans@yahoo.com",
+        "movie_id": "783925f8-4173-4cae-a6f5-9756de33e848",
+        "text": "Floor or wait system necessary summer. Election less stop onto social fine me thank.",
+        "date": "2021-08-20T01:17:25"
+    },
+    {
+        "name": "Brittany Logan",
+        "email": "geoffreysmith@gmail.com",
+        "movie_id": "fa9eb0e4-3e53-4f28-aef7-1ec7f1345ef8",
+        "text": "Themselves mind they. Since each little third program.",
+        "date": "2021-04-03T21:44:56"
+    },
+    {
+        "name": "Kelly Stewart",
+        "email": "johnellis@shelton-warren.com",
+        "movie_id": "c43df16f-f10b-4425-ab6f-885720970240",
+        "text": "Student nothing dark heavy environment child.",
+        "date": "2020-10-19T14:25:11"
+    },
+    {
+        "name": "Danielle Campbell",
+        "email": "lwilliams@brown.com",
+        "movie_id": "a113e681-9cf4-4057-a84d-3aa48691317d",
+        "text": "Full cup candidate film glass seek. Opportunity others appear life.",
+        "date": "2020-04-14T09:41:48"
+    },
+    {
+        "name": "Corey Fowler",
+        "email": "bethreynolds@gmail.com",
+        "movie_id": "672e178f-76be-4a55-b899-b16547d6fbd4",
+        "text": "Offer successful hot whole image film example realize.",
+        "date": "2019-07-11T18:32:16"
+    },
+    {
+        "name": "Sara Hamilton",
+        "email": "swheeler@hotmail.com",
+        "movie_id": "1f718497-5978-4ff8-b91d-071b3c96a4e9",
+        "text": "Responsibility sister easy style song ability trouble very.",
+        "date": "2019-12-27T04:47:06"
+    },
+    {
+        "name": "George Patterson",
+        "email": "rebeccahill@reynolds.com",
+        "movie_id": "9f18717b-ec1e-4e33-a4c4-626b6f3e1c1c",
+        "text": "Music type bring firm affect appear anyone.",
+        "date": "2021-02-14T07:28:54"
+    },
+    {
+        "name": "Victoria Rodriguez",
+        "email": "teresaross@hotmail.com",
+        "movie_id": "a2d58d7d-e8d3-493e-9b45-fb785dc92e94",
+        "text": "Suddenly store future another to audience discussion idea happy.",
+        "date": "2022-06-23T16:39:43"
+    },
+    {
+        "name": "Eugene Haynes",
+        "email": "mathewsgarcia@gmail.com",
+        "movie_id": "43b15f7e-49e2-4b4a-8b31-2ec54e663c6d",
+        "text": "Either body explain others. Main station fear either traditional.",
+        "date": "2020-03-05T20:58:47"
+    },
+    {
+        "name": "David Rivera",
+        "email": "robertsandra@yahoo.com",
+        "movie_id": "98e935ff-9a88-45b8-a42d-79217bb2eb80",
+        "text": "Rule recognize beyond describe. Yourself treat least beautiful court.",
+        "date": "2021-11-01T19:18:33"
+    },
+    {
+        "name": "Christine White",
+        "email": "kennethbrown@holmes.com",
+        "movie_id": "f62dc49b-75ff-4307-b116-2c518d417b8b",
+        "text": "Write person over north benefit contain. Cup talk own way.",
+        "date": "2019-10-21T08:51:45"
+    },
+    {
+        "name": "Joshua Bishop",
+        "email": "vickytaylor@lewis-martin.com",
+        "movie_id": "69c9b9f6-48e7-4b36-803f-134e4c8dc3b2",
+        "text": "South occur tend despite simple. Red major leg there individual opportunity raise.",
+        "date": "2022-08-05T14:08:17"
+    },
+    {
+        "name": "Lisa Kim",
+        "email": "phillipswilliam@hotmail.com",
+        "movie_id": "a1144b60-dfe1-4d38-b768-0ccda23a6599",
+        "text": "Technology bad laugh against. Pressure party support history your question.",
+        "date": "2020-05-22T02:18:42"
+    },
+    {
+        "name": "Bobby Lee",
+        "email": "kimwhite@yahoo.com",
+        "movie_id": "9c6fb014-0864-4686-bd0b-09fc42e73c92",
+        "text": "World clear close already writer. Next myself property.",
+        "date": "2021-03-15T11:12:33"
+    },
+    {
+        "name": "Jennifer Lopez",
+        "email": "stephaniebarnett@hotmail.com",
+        "movie_id": "c0033a9d-1d63-4e8f-9b44-f5930e91db23",
+        "text": "Throw little floor husband. Forward take clear morning school fast.",
+        "date": "2021-10-07T17:30:12"
+    },
+    {
+        "name": "Jessica Ward",
+        "email": "lewisjohn@owens.net",
+        "movie_id": "3f59e9fb-6192-4662-832a-46285aeb5f31",
+        "text": "Team relate such them cut rise. Air important data particularly director.",
+        "date": "2020-12-18T06:09:29"
+    },
+    {
+        "name": "Deborah Myers",
+        "email": "brownelizabeth@gmail.com",
+        "movie_id": "4a203485-6599-4e2f-a2da-1c6f6d5b9c8d",
+        "text": "Arrive society each mission front individual. Word part book knowledge.",
+        "date": "2021-09-21T12:15:42"
+    },
+    {
+        "name": "Henry Coleman",
+        "email": "melissa98@hotmail.com",
+        "movie_id": "f2783b13-7b19-4903-8f44-15cfd702d00c",
+        "text": "Send reality fear single part. Garden play financial except new.",
+        "date": "2021-12-25T10:24:06"
+    }
+]
