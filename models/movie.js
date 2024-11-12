3805bc3e413614d4027d7e9eeef24bb88c2b26b1
ShaVon H. <144467852+brownsugarcoder@users.noreply.github.com>
2024-11-11 19:28:08 -0500

    Update movie.js

--- models/movie.js
+++ models/movie.js
@@ -1,13 +1,1 @@
-const mongoose = require('mongoose');
 
-// Define the schema for a Movie
-const movieSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  genre: { type: String, required: true },
-  awards: { type: Number, default: 0 }
-});
-
-// Create the Movie model
-const Movie = mongoose.model('Movie', movieSchema);
-
-module.exports = Movie;
